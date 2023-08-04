6a5b77f42c2ab4ebdd9ca089e831abc3d5b94ebf
mstiles-grs <mstiles@grs-usa.com>
2023-08-04 08:46:35 -0500

    testing out

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    let name: string = 'world';
+
+    function greet(name: string) {
+        alert(`Hello, ${name}!`);
+    }
+</script>
+
+<div>
+    <button on:click={() => greet(name)}>Greet {name}</button>
+</div>
